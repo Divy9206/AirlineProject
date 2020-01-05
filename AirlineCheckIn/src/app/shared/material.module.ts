68fe9574f149171e8b32cfe39438e03ae8b81cce
divyanshu <divyanshu.imp7@gmail.com>
2020-01-05 20:05:15 +0530

    Added side navbar

--- AirlineCheckIn/src/app/shared/material.module.ts
+++ AirlineCheckIn/src/app/shared/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @NgModule({
   declarations: [],
   exports: [
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }

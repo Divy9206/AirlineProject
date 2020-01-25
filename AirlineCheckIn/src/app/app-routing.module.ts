9b9d5ea3d360bf40d981a4e6dce8dc0c2da62e07
Divy9206 <47363670+Divy9206@users.noreply.github.com>
2020-01-25 23:16:03 +0530

    Added Passenger List and Passenger Detail component

--- AirlineCheckIn/src/app/app-routing.module.ts
+++ AirlineCheckIn/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthorizeAppGuard } from './core/authorize-app.guard';
+import { PassengerDetailsComponent } from './passenger/passenger-details/passenger-details.component';
+import { PassengerListComponent } from './passenger/passenger-list/passenger-list.component';
 
 const routes: Routes = [
     {
@@ -11,6 +13,14 @@ const routes: Routes = [
     {
         path: 'landing-page',
         loadChildren: () => import('./core/core.module').then(mod => mod.CoreModule),
+    },
+    {
+        path: 'passenger-details',
+        component: PassengerDetailsComponent
+    },
+    {
+        path: 'passenger-list',
+        component: PassengerListComponent
     }
 ];
 
